8467f9565ed517b358496a83318fd96faecd5b47
Mikaell Victor <mikaell.victor@hotmail.com>
2025-06-22 20:16:19 +0000

    Inclusão da recursividade

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,37 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+// Recursivo, movimentar peça conforme valor inserido
+void moverbispo (int bispo){ 
+    if (bispo>0){
+        moverbispo (bispo - 1);
+        printf("Bispo Branco se movimentou para a diagonal superior direita, (%d) casa.\n", bispo);
+    }
+}
+// Recursivo, movimentar peça conforme valor inserido
+void movertorre (int torre){ 
+    if (torre>0){
+        movertorre (torre - 1);
+        printf("Torre Branca se movimentou para cima, (%d) casa.\n", torre);
+    }
+}
+// Recursivo, movimentar peça conforme valor inserido
+void moverrainha (int rainha){ 
+    if (rainha>0){
+        moverrainha (rainha - 1);
+        printf("Rainha Branca se movimentou para a esquerda, (%d) casa.\n", rainha);
+    }
+}
+// Recursivo, movimentar peça conforme valor inserido
+void movercavalo (int cavalo){ 
+    if (cavalo>0){
+        movercavalo (cavalo - 1);
+        for (int i = 1; i < 3; i++){
+            printf("Cavalo Branco fez o movimento em 'L' para cima, (%d) casa.\n",i);
+        }
+        printf("Cavalo Branco fez o movimento em 'L' para direta, (%d) casa.\n", cavalo);
+    }
+}
 int main()
 {
 
@@ -23,35 +53,39 @@ int main()
             switch (opcao){
             case 1:
                 // Movimentação do Bispo
-                for (int i = 0; i < 5; i++){
+                moverbispo(5);
+                /*for (int i = 0; i < 5; i++){
                 printf("Bispo Branco se movimentou para a diagonal superior direita, (%d) casa.\n", i + 1);
-                }
+                }*/
             break;
 
             case 2:
                 // Movimentação da Torre
-                for (int i = 0; i < 6; i++){
+                movertorre(6);
+                /*for (int i = 0; i < 6; i++){
                 printf("Torre Branca se movimentou para cima, (%d) casa.\n", i + 1);
-                }
+                }*/
             break;
 
             case 3:
                 // Movimentação da Rainha
-                for (int i = 0; i < 8; i++){
+                moverrainha(8);
+                /*for (int i = 0; i < 8; i++){
                 printf("Rainha Branca se movimentou para a esquerda, (%d) casa.\n", i + 1);
-                }
+                }*/
             break;
 
             case 4:
                 // Movimentação do Cavalo
-                for (int i = 0; i < 1; i++){
+                movercavalo(1);
+                /*for (int i = 0; i < 1; i++){
                     for (int i = 0; i < 2; i++)
                     {
                         printf("Cavalo Branco fez o movimento em 'L' para baixo, (%d) casa.\n");
                     }
                     
                 printf("Cavalo Branco fez movimento em 'L' para esquerda, (%d) casa.\n", i + 1);
-                }
+                }*/
             break;
 
             case 5:
@@ -68,10 +102,6 @@ int main()
 
     return 0;
 }
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
